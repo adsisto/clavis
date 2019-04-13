241752a4b81c92afbe335682238baa1ebf7f1abb
Andrew Ying <hi@andrewying.com>
2019-04-13 21:21:33 +0100

    Update front end config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-const { app, BrowserWindow } = require('electron');
+/*
+ * Copyright (c) Andrew Ying 2019.
+ *
+ * This file is part of Clavis.
+ *
+ * Clavis is free software. You can use, share, and build it under the terms of the
+ * API Copyleft License. As far as the law allows, this software comes as is, without
+ * any warranty or condition, and no contributor will be liable to anyone for any
+ * damages related to this software or this license, under any kind of legal claim.
+ *
+ * A copy of the API Copyleft License is available at <LICENSE.md>.
+ */
+
+const { app, BrowserWindow, Menu, Tray } = require('electron');
+const Store = require('electron-store');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+const storeSchema = {
+  ready: { type: 'boolean', default: false },
+  startOnBoot: { type: 'boolean', default: true },
+  hiddenOnStart: { type: 'boolean', default: true },
+  identity: { type: 'string', format: 'email' },
+  hotKey: { type: 'string' }
+};
 
-const createWindow = () => {
+// Keep a global reference of the window object
+let store;
+let mainWindow;
+let tray;
+
+store = new Store({
+  schema: storeSchema
+});
+app.setLoginItemSettings({
+  openAtLogin: store.get('startOnBoot', true),
+  openAsHidden: store.get('hiddenOnStart', true),
+});
+
+const setupWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
@@ -31,27 +62,48 @@ const createWindow = () => {
   });
 };
 
+const trayMenu = Menu.buildFromTemplate([
+  {
+    label: 'Open',
+    type: 'normal',
+    click: () => {
+      if (mainWindow === null) {
+        setupWindow();
+      }
+    }
+  },
+  {
+    label: 'About',
+    type: 'normal'
+  }
+]);
+
+const setUpTray = () => {
+  // Create the tray menu.
+  tray = new Tray(`${__dirname}/images/tray/icon.png`);
+  tray.setToolTip(app.getName());
+  tray.setContextMenu(trayMenu);
+
+  // Create event listener.
+  tray.on('click', () => {
+    this.popUpContextMenu();
+  });
+};
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
+app.on('ready', () => {
+  if (process.platform === 'darwin') {
+    app.dock.hide();
   }
+
+  setUpTray();
+  setupWindow();
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    setupWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
