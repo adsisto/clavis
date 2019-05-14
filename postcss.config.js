492795ee56b4dead914a153cf0bbb24fd201fb2b
Andrew Ying <hi@andrewying.com>
2019-05-14 16:53:12 +0100

    Rename main process file and update formatting

--- postcss.config.js
+++ postcss.config.js
@@ -4,11 +4,12 @@
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of version 3 of the GNU General Public License as published by the
- * Free Software Foundation.
+ * Free Software Foundation. In addition, this program is also subject to certain
+ * additional terms available at <SUPPLEMENT.md>.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <https://www.gnu.org/licenses/>.
@@ -16,9 +17,9 @@
 
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
-      browsers: "electron 4.1"
+    'postcss-import': {},
+    'postcss-preset-env': {
+      browsers: 'electron 5.0'
     },
     autoprefixer: {}
   }
--- webpack.config.js
+++ webpack.config.js
@@ -4,54 +4,55 @@
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of version 3 of the GNU General Public License as published by the
- * Free Software Foundation.
+ * Free Software Foundation. In addition, this program is also subject to certain
+ * additional terms available at <SUPPLEMENT.md>.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
-const ExtractCssChunks = require("extract-css-chunks-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const HtmlPlugin = require("html-webpack-plugin");
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
-const production = process.env.NODE_ENV === "production";
+const production = process.env.NODE_ENV === 'production';
 
 let config = {
-  mode: production ? "production" : "development",
+  mode: production ? 'production' : 'development',
   node: {
     __dirname: false
   },
-  context: path.resolve(__dirname, "src"),
-  target: "electron-main",
+  context: path.resolve(__dirname, 'src'),
+  target: 'electron-main',
   entry: {
-    index: "./index.js",
-    app: "./app.js",
-    about: "./about.js"
+    main: './main.js',
+    app: './app.js',
+    about: './about.js'
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.(jpe?g|png|gif|ttf|svg)$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[path][name].[ext]"
+            name: '[path][name].[ext]'
           },
         }
       },
@@ -64,17 +65,17 @@ let config = {
               hot: !production
             }
           },
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
         ]
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.css', '.scss'],
+    extensions: [ '.js', '.jsx', '.css', '.scss' ],
     alias: {
-      "~": path.resolve(__dirname, "node_modules")
+      '~': path.resolve(__dirname, 'node_modules')
     }
   },
   plugins: [
@@ -82,19 +83,19 @@ let config = {
       maxChunks: 1
     }),
     new ExtractCssChunks(
-      { filename: "[name].css" }
+      { filename: '[name].css' }
     ),
     new HtmlPlugin({
-      title: "Clavis",
+      title: 'Clavis',
       template: 'index.html',
-      chunks: ["app"]
+      chunks: [ 'app' ]
     }),
     new HtmlPlugin({
-      title: "",
-      template: "about.html",
-      filename: "about.html",
-      chunks: ["about"],
-      config: require("./package.json")
+      title: '',
+      template: 'about.html',
+      filename: 'about.html',
+      chunks: [ 'about' ],
+      config: require('./package.json')
     })
   ],
   optimization: {
@@ -106,7 +107,7 @@ let config = {
 if (production) {
   config.plugins.push(
     new webpack.DefinePlugin({
-      "process.env": { NODE_ENV: JSON.stringify("production") }
+      'process.env': { NODE_ENV: JSON.stringify('production') }
     })
   );
   config.optimization.minimizer.push(new TerserPlugin({
@@ -115,7 +116,7 @@ if (production) {
     sourceMap: true
   }));
 } else {
-  config.devtool = "source-map";
+  config.devtool = 'source-map';
 }
 
 module.exports = config;
